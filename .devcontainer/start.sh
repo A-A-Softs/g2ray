c44dedbb6de8c4c3bc596b7c4e035efca7d43ab4
osumyy <53944576+A-A-Softs@users.noreply.github.com>
2026-05-11 23:49:59 +0000

    Update .devcontainer/start.sh

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# g2ray start script
 tmux kill-session -t g2ray 2>/dev/null || true
 tmux new-session -d -s g2ray
 tmux send-keys -t g2ray "sudo /usr/local/bin/xray run -c /etc/xray/g2ray.json &>/tmp/xray.log" Enter
